3fd8060cf1b872f0456ac5800e8e7234cbf1d802
Pitchayasak Senachai <pitchayasak.s@gmail.com>
2019-11-12 16:06:15 +0700

    edit description

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(
-    name="torchdiffeq",
+    name="torchdiffeqduke",
     version="0.0.1",
     author="Ricky Tian Qi Chen",
     author_email="rtqichen@cs.toronto.edu",
-    description="ODE solvers and adjoint sensitivity analysis in PyTorch.",
+    description="ODE solvers and adjoint sensitivity analysis in PyTorch. (fork to sertis)",
     url="https://github.com/rtqichen/torchdiffeq",
     packages=['torchdiffeq', 'torchdiffeq._impl'],
     install_requires=['torch>=0.4.1'],
